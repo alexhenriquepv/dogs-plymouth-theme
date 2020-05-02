569c96881e7d58762ea7530caccbc51e89373520
Alex Paiva <alexhenriquepv@gmail.com>
2020-05-01 21:48:08 -0300

    Inicializando o repositório

---
+++ apply-theme.sh
@@ -0,0 +1,4 @@
+#sudo plymouth-set-default-theme -l
+
+# --rebuild-initrd atualiza as configurações do kernel
+sudo plymouth-set-default-theme dogs --rebuild-initrd
---
+++ gif-to-png.sh
@@ -0,0 +1,6 @@
+
+# Cria o diretório, se não existe
+mkdir -p img
+
+# API do ImageMagick
+convert ./theme.gif ./img/progress-%d.png
